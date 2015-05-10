958c4dad4c4d33fedccdcf6c61099a7bb7cc540a
Kinjalc <kinjal.chauhan@gmail.com>
2015-05-10 10:47:51 -0400

    commit

--- app_ttt.js
+++ app_ttt.js
@@ -1,22 +1,8 @@
-// var grid = {
-//   $('#a1') : "null";
-//   $('#a2') : "null";
-//   $('#a3') : "null";
-//   $('#a4') : "null";
-//   $('#a5') : "null";
-//   $('#a6') : "null";
-//   $('#a7') : "null";
-//   $('#a8') : "null";
-//   $('#a9') : "null";
-
-// }
-
-
-
-
-
 var player1 = 0;
 var player2 = 1;
+var storeMoves ={
+
+}
 
 var selectPlayer=function(sqr){
     if (player1===0){
@@ -24,30 +10,31 @@ var selectPlayer=function(sqr){
       sqr.text("X");
       $('#player').text("player2 turn");
       player1 = 1-player1;
-      player2 = 1-player2;
-    };
+      alert("player1 clicked " + sqr.attr('id'));
+      storeMoves[sqr.attr('id')] = "X";
+
+    }
 
     else {
       sqr.text("O");
       $('#player').text("player1 turn");
       player1 = 1-player1;
-      player2 = 1-player2;
-    };
+      alert("player2 clicked " + sqr.attr('id'));
+      storeMoves[sqr.attr('id')] = "O";
+    }
 
-
+console.log(storeMoves);
 
 }
 
 
 $(document).ready(function(){
   $('.square').on('click',function(e) {
-  // if (grid[$(this)]==="null"){
    ($(this)).removeClass('square');
    ($(this)).addClass('clicked');
    // $('#player').text("player2 turn");
     /* Act on the event */
     selectPlayer($(this));
-
     });
-  // };
+
 })
