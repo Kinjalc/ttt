83c4e34b1634dd76618808a7d17c22791e3d33ac
Kinjalc <kinjal.chauhan@gmail.com>
2015-05-12 11:31:28 -0400

    commit with end game

--- app_ttt.js
+++ app_ttt.js
@@ -12,7 +12,10 @@ var selectPlayer=function(sqr){
       player1 = 1-player1;
       //alert("player1 clicked " + sqr.attr('id'));
       storeMoves[sqr.attr('id')] = "X";
-      winTheGame(sqr.attr('id'));
+      // winTheGame(sqr.attr('id'))
+      if (winTheGame(sqr.attr('id')) === true){
+        $(".square").off("click");
+      }
       }
 
   else {
@@ -32,13 +35,21 @@ console.log(storeMoves);
 
 
 $(document).ready(function(){
+  var count = 0
   $('.square').on('click',function(e) {
     if (storeMoves[$(this).attr('id')]===undefined){
+      count ++;
       ($(this)).removeClass('square');
       ($(this)).addClass('clicked');
       selectPlayer($(this));
-      };
-    })
+
+      if(count===9 ){
+        $(".square").off("click");
+        alert("It's a tie!")
+      }
+
+    }
+  })
 
 
 
--- ttt.js
+++ ttt.js
@@ -1,42 +1,19 @@
 var winTheGame= function(playerSquare){
   var win = false;
   storeMoves[playerSquare]=== player;
-    if(storeMoves['a1']===player && storeMoves['a2']===player && storeMoves['a3']===player) {
-      win  = true;
-      alert("player wins!");
-    }
-    else if (storeMoves['a4']===player && storeMoves['a5']===player && storeMoves['a6']===player) {
-      win  = true;
-      alert("player wins!")
-    }
-    else if (storeMoves['a1']===player && storeMoves['a2']===player && storeMoves['a3']===player) {
-      win  = true;
-      alert("player wins!")
-    }
-    else if (storeMoves['a1']===player&& storeMoves['a4']===player && storeMoves['a7']===player) {
-      win  = true;
-      alert("player wins!")
-    }
-    else if (storeMoves['a2']===player && storeMoves['a5']===player && storeMoves['a8']===player) {
-      win  = true;
-     alert("player wins!")
-    }
-    else if (storeMoves['a3']===player && storeMoves['a6']===player && storeMoves['a9']===player) {
-      win  = true;
-      alert("player wins!")
-    }
-    else if (storeMoves['a1']===player && storeMoves['a5']===player && storeMoves['a9']===player) {
-      win  = true;
-      alert("player wins!")
-    }
-    else if (storeMoves['a3']===player && storeMoves['a5']===player && storeMoves['a7']===player) {
-      win  = true;
-      alert("player wins!")
-    }
-
-  if (win===true){
+  if ((storeMoves['a1']===player && storeMoves['a2']===player && storeMoves['a3']===player) ||
+      (storeMoves['a4']===player && storeMoves['a5']===player && storeMoves['a6']===player) ||
+      (storeMoves['a1']===player && storeMoves['a2']===player && storeMoves['a3']===player) ||
+      (storeMoves['a1']===player && storeMoves['a4']===player && storeMoves['a7']===player) ||
+      (storeMoves['a2']===player && storeMoves['a5']===player && storeMoves['a8']===player) ||
+      (storeMoves['a3']===player && storeMoves['a6']===player && storeMoves['a9']===player) ||
+      (storeMoves['a1']===player && storeMoves['a5']===player && storeMoves['a9']===player) ||
+      (storeMoves['a3']===player && storeMoves['a5']===player && storeMoves['a7']===player)) {
+    win  = true;
+    alert(player + " wins!");
     alert("Game ends!")
   }
+
   return win;
 
 }
