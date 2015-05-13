e6335dada592e342862fbd2941cf0fbdeb52ae9a
Kinjalc <kinjal.chauhan@gmail.com>
2015-05-13 09:35:59 -0400

    commit with endgame and reset error fixed

--- app_ttt.js
+++ app_ttt.js
@@ -2,11 +2,13 @@ var player1 = 0 ;
 var player2 = 1 ;
 var player;
 var storeMoves={};
+var gameOver = false;
 
 var selectPlayer=function(sqr){
   if (player1===0){
 
-      sqr.text("X");
+      sqr.text("X").addClass('animated rotateIn');
+      // $(sqr).addClass('animated rotateIn');
       player="X";
       $('#player').text("player 2 turn");
       player1 = 1-player1;
@@ -14,12 +16,16 @@ var selectPlayer=function(sqr){
       storeMoves[sqr.attr('id')] = "X";
       // winTheGame(sqr.attr('id'));
       if (winTheGame(sqr.attr('id')) === true){
-        $(".square").off("click");
+        gameOver = true;
+        //$('.square').addClass('clicked');
+        //$('.clicked').removeClass('square');
+
+        //$(".square").off("click");
       }
       }
 
   else {
-      sqr.text("O");
+      sqr.text("O").addClass('animated zoomIn');
       player="O";
       $('#player').text("player 1 turn");
       player1 = 1-player1;
@@ -27,7 +33,11 @@ var selectPlayer=function(sqr){
       storeMoves[sqr.attr('id')] = "O";
       // winTheGame(sqr.attr('id'));
       if (winTheGame(sqr.attr('id')) === true){
-        $(".square").off("click");
+        gameOver = true;
+
+        //$('.square').addClass('clicked');
+        //$('.clicked').removeClass('square');
+        //$(".square").off("click");
       }
     }
 
@@ -37,12 +47,13 @@ console.log(storeMoves);
 
 function endGame(){
   $('.clicked').text(" ");
+  // $('.square').addClass('clicked');
   $('.clicked').addClass('square');
-  $('.square').addClass('square');
   $('.square').removeClass('clicked');
   storeMoves={};
   player1=0;
   player2=1;
+  gameOver = false;
   $('#player').text("Player 1 plays");
 }
 
@@ -51,6 +62,10 @@ function endGame(){
 $(document).ready(function(){
   var count = 0;
   $('.square').on('click',function(e) {
+    if (gameOver === true) {
+      alert("Game has ended");
+      return;
+    }
     if (storeMoves[$(this).attr('id')]===undefined){
       count ++;
       ($(this)).removeClass('square');
